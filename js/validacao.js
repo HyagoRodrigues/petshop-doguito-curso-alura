ff54e288207002c3138e3a4b24f63fb94e7274f1
Hyago Rodrigues <hyago-rodrigues@live.com>
2022-05-31 17:38:29 -0300

    validando a data

---
+++ js/validacao.js
@@ -0,0 +1,29 @@
+
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+    }
+    
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
+    if(!maiorQue18(dataRecebida)){
+        mensagem = 'A idade mínima é de 18 anos';
+    }
+    input.setCustomValidity(mensagem);
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+}
